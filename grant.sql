5f7f04d9c300aad216a80a7528a0a34eb396dd99
Daniel Black <grooverdan@users.sourceforge.net>
2015-09-14 12:04:51 +1000

    add country to grant in client

--- grant.sql
+++ grant.sql
@@ -1,3 +1,3 @@
-grant all on *.* to 'travis_ssl' IDENTIFIED BY 'travis_ssl' REQUIRE ISSUER '/C=XX/O=Travis Test MySQL CA server/CN=Travis MySQL CA' AND SUBJECT '/O=Travis MySQL Client/CN=Travis MySQL client';
+grant all on *.* to 'travis_ssl' IDENTIFIED BY 'travis_ssl' REQUIRE ISSUER '/C=XX/O=Travis Test MySQL CA server/CN=Travis MySQL CA' AND SUBJECT '/C=XX/O=Travis MySQL Client/CN=Travis MySQL client';
 grant all on *.* to 'travis_ssl_i' IDENTIFIED BY 'travis_ssl' REQUIRE ISSUER '/C=XX/O=Travis Test MySQL CA server/CN=Travis MySQL CA';
-grant all on *.* to 'travis_ssl_s' IDENTIFIED BY 'travis_ssl' REQUIRE SUBJECT '/O=Travis MySQL Client/CN=Travis MySQL client';
+grant all on *.* to 'travis_ssl_s' IDENTIFIED BY 'travis_ssl' REQUIRE SUBJECT '/C=XX/O=Travis MySQL Client/CN=Travis MySQL client';
