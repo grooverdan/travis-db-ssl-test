5fce171f63d7333ff317be0f2ba77bbabba8c713
Daniel Black <grooverdan@users.sourceforge.net>
2015-09-15 21:40:05 +1000

    many corrections

--- grant.sql
+++ grant.sql
@@ -1,3 +1,1 @@
-grant all on *.* to 'travis_ssl' IDENTIFIED BY 'travis_ssl' REQUIRE ISSUER '/C=XX/O=Travis Test MySQL CA server/CN=Travis MySQL CA' AND SUBJECT '/C=XX/O=Travis MySQL Client/CN=Travis MySQL client';
-grant all on *.* to 'travis_ssl_i' IDENTIFIED BY 'travis_ssl' REQUIRE ISSUER '/C=XX/O=Travis Test MySQL CA server/CN=Travis MySQL CA';
-grant all on *.* to 'travis_ssl_s' IDENTIFIED BY 'travis_ssl' REQUIRE SUBJECT '/C=XX/O=Travis MySQL Client/CN=Travis MySQL client';
+grant all on *.* to 'travis_ssl' IDENTIFIED BY 'pass' REQUIRE ISSUER '/CN=Travis MySQL CA/O=Travis' AND SUBJECT '/CN=Travis MySQL client/O=Travis';
