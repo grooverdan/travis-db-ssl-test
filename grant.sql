52a1c142d5f46dab04e1a16e4011639ce8af094a
Daniel Black <grooverdan@users.sourceforge.net>
2015-09-16 10:50:08 +1000

    O before CN

--- grant.sql
+++ grant.sql
@@ -1,1 +1,1 @@
-grant all on *.* to 'travis_ssl' IDENTIFIED BY 'pass' REQUIRE ISSUER '/CN=Travis MySQL CA/O=Travis' AND SUBJECT '/CN=Travis MySQL client/O=Travis';
+grant all on *.* to 'travis_ssl' IDENTIFIED BY 'pass' REQUIRE ISSUER '/O=Travis/CN=Travis MySQL CA' AND SUBJECT '/O=Travis/CN=Travis MySQL client';
